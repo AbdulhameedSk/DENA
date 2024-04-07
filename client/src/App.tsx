84b0339cda8be27e2eb03e2445b9a7d3a257d2d7
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-04-07 16:28:53 +0530

    Completed routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Signup from "./pages/Signup";
+import SignIn from "./pages/Signin";
+import Categories from "./pages/Categories";
+import FriendSuggestions from "./pages/FriendsSuggestions";
+import { useEffect, useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Box, Modal, Typography } from "@mui/material";
+
+const denaurlenTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#4B0082",
+    },
+    background: {
+      paper: "#F7F2FF",
+      default: "#FFFFFF",
+    },
+    text: {
+      primary: "#343434",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+    fontWeightRegular: 600,
+    button: {
+      textTransform: "none",
+    },
+  },
+});
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: "10px",
+  p: 4,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [verified, setVerified] = useState<any>(false);
+  const [modal, setModal] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    const jsonValue = localStorage.getItem("denaurlen-token");
+
+    const verifyUser = async (arg: string) => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_backend_url + "/verify",
+          {
+            headers: { Authorization: `Bearer ${arg}` },
+          }
+        );
+        await response.json();
+      } catch (error) {
+        console.error(error);
+        setOpen(true);
+        setModal("something went wrong");
+      }
+    };
+
+    if (jsonValue != null && jsonValue != "undefined") {
+      const isVerified = verifyUser(JSON.parse(jsonValue));
+      setVerified(isVerified);
+    }
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={denaurlenTheme}>
+      <Routes>
+        <Route
+          path="/"
+          element={verified ? <Navigate to="/friends" /> : <Welcome />}
+        />
+        <Route
+          path="/signup"
+          element={verified ? <Navigate to="/friends" /> : <Signup />}
+        />
+        <Route
+          path="/signin"
+          element={verified ? <Navigate to="/friends" /> : <SignIn />}
+        />
+        <Route path="/interests" element={<Categories />} />
+        <Route path="/friends" element={<FriendSuggestions />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      {modal && (
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {modal}
+            </Typography>
+          </Box>
+        </Modal>
+      )}
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
